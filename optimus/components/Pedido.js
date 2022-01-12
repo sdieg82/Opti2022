a31672c4de666ff444428747130410f8d08db077
Diego <sdieg82@gmail.com>
2022-01-11 19:39:03 -0500

    Opti Oficial

--- optimus/components/Pedido.js
+++ optimus/components/Pedido.js
@@ -190,7 +190,7 @@ const Pedido = ({pedido}) => {
                 <h2 className="text-gray-800 font-bold mt-3">Fecha de compra</h2>
                 {newDate && (
                     <p className="flex items-center my-2">
-                      <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
+                      <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z "></path></svg>
                        {newDate}
                     </p>
                 )}
